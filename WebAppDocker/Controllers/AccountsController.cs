c944a408c0cb757784928b657a607645b1bdf36c
Gnanaprasuna <ittamgnanaprasuna1755@gmail.com>
2024-02-26 12:33:53 +0530

    Add project files.

---
+++ WebAppDocker/Controllers/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDocker.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
